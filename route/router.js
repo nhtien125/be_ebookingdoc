8658abcb2c06cc95a0b369b3d393871fe825da5f
tiennh <177102287+nhtien125@users.noreply.github.com>
2025-06-14 22:07:38 +0700

    update

--- route/router.js
+++ route/router.js
@@ -7,20 +7,13 @@ const doctorController = require('../src/controller/doctorController');
 const SpecializationController = require('../src/controller/specializationController');
 const clinicController = require('../src/controller/clinicController');
 const medicalRecordController = require('../src/controller/medicalRecordController');
-const newsController = require('../src/controller/newsController');
-const packageController = require('../src/controller/packageController');
 const patientController = require('../src/controller/patientController');
 const premissionController = require('../src/controller/premissionController');
-const resultController = require('../src/controller/resultController');
 const reviewController = require('../src/controller/reviewController');
 const scheduleController = require('../src/controller/scheduleController');
 const hospitalController = require('../src/controller/hospitalController');
 const multer = require('multer');
 const upload = multer({ dest: 'uploads/' });
-const hospitalSpecializationController = require("../src/controller/hospitalSpecializationController");
-const hospitalDoctorController = require("../src/controller/hospitalDoctorController");
-const clinicSpecializationController = require("../src/controller/clinicSpecializationController");
-const clinicDoctorController = require("../src/controller/clinicDoctorController");
 const MedicalServiceController = require("../src/controller/medicalserviceController");
 const SelectProfileController = require("../src/controller/SelectProfileController");
 const ArticleController = require("../src/controller/ArticleController");
@@ -84,22 +77,6 @@ router.post(`/${medicalRecord}/add`, medicalRecordController.create);
 router.put(`/${medicalRecord}/update/:id`, medicalRecordController.update);
 router.delete(`/${medicalRecord}/delete/:id`, medicalRecordController.delete);
 
-// News
-const news = "news";
-router.get(`/${news}/getAll`, newsController.getAll);
-router.get(`/${news}/getById/:id`, newsController.getById);
-router.post(`/${news}/add`, newsController.create);
-router.put(`/${news}/update/:id`, newsController.update);
-router.delete(`/${news}/delete/:id`, newsController.delete);
-
-//Package
-const packageRoute = "package";
-router.get(`/${packageRoute}/getAll`, packageController.getAll);
-router.get(`/${packageRoute}/getById/:id`, packageController.getById);
-router.post(`/${packageRoute}/add`, packageController.create);
-router.put(`/${packageRoute}/update/:id`, packageController.update);
-router.delete(`/${packageRoute}/delete/:id`, packageController.delete);
-
 // Patient
 const patient = "patient";
 router.get(`/${patient}/getAll`, patientController.getAll);
@@ -115,14 +92,6 @@ router.get(`/${permission}/getById/:id`, premissionController.getById);
 router.post(`/${permission}/add`, premissionController.create);
 router.put(`/${permission}/update/:id`, premissionController.update);
 router.delete(`/${permission}/delete/:id`, premissionController.delete);
-
-// Result
-const result = "result";
-router.get(`/${result}/getAll`, resultController.getAll);
-router.get(`/${result}/getById/:id`, resultController.getById);
-router.post(`/${result}/add`, resultController.create);
-router.put(`/${result}/update/:id`, resultController.update);
-router.delete(`/${result}/delete/:id`, resultController.delete);
 
 // Review
 const review = "review";
@@ -147,27 +116,6 @@ router.get(`/${hospital}/getById/:id`, hospitalController.getById);
 router.post(`/${hospital}/add`, upload.single('image'), hospitalController.create);
 router.put(`/${hospital}/update/:id`, upload.single('image'), hospitalController.update);
 router.delete(`/${hospital}/delete/:id`, hospitalController.delete);
-
-const hospital_specializations = "hospital_specializations";
-router.post(`/${hospital_specializations}/add`, hospitalSpecializationController.addSpecialization);
-router.delete(`/${hospital_specializations}/delete`, hospitalSpecializationController.removeSpecialization);
-router.get(`/${hospital_specializations}/getById/:hospital_uuid`, hospitalSpecializationController.getSpecializationsByHospital);
-
-const hospital_doctors = "hospital_doctors";
-router.post(`/${hospital_doctors}/add`, hospitalDoctorController.addDoctor);
-router.delete(`/${hospital_doctors}/delete`, hospitalDoctorController.removeDoctor);
-router.get(`/${hospital_doctors}/getById/:hospital_uuid`, hospitalDoctorController.getDoctorsByHospital);
-
-const clinic_specializations = "clinic_specializations";
-router.post(`/${clinic_specializations}/add`, clinicSpecializationController.addSpecialization);
-router.delete(`/${clinic_specializations}/delete`, clinicSpecializationController.removeSpecialization);
-router.get(`/${clinic_specializations}/getById/:clinic_uuid`, clinicSpecializationController.getSpecializationsByClinic);
-
-const clinic_doctors = "clinic_doctors";
-router.post(`/${clinic_doctors}/add`, clinicDoctorController.addDoctor);
-router.delete(`/${clinic_doctors}/delete`, clinicDoctorController.removeDoctor);
-router.get(`/${clinic_doctors}/getById/:clinic_uuid`, clinicDoctorController.getDoctorsByClinic);
-
 
 const medical_service = "medical_service";
 router.get(`/${medical_service}/getAll`, MedicalServiceController.getAll);
